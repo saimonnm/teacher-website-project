97e8adb724e27a0d9ba60e2fbfb1e6c25f03edf5
Simona Mangano <simona_mangano99@yahoo.it>
2025-07-15 23:09:18 +0200

    feat(config): raw config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./components/home/home').then((x) => x.Home),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'who-i-am',
+    loadComponent: () =>
+      import('./components/who-i-am/who-i-am').then((x) => x.WhoIAm),
+  },
+];
