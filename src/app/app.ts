b3cc5826574a5e63f170c5e1a72959129f4cbe62
Simona Mangano <simona_mangano99@yahoo.it>
2025-07-15 23:07:01 +0200

    feat(config): home page config

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from './components/footer/footer';
+import { HeaderNav } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderNav, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
