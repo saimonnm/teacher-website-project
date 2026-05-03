8c2c0c6bb6d84d93e27dd1f75f19274ef9d6408d
Simona Mangano <simona_mangano99@yahoo.it>
2026-05-03 22:27:39 +0200

    improved login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  imports: [ReactiveFormsModule, RouterLink],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  loginForm = new FormGroup({
+    username: new FormControl('', { validators: [Validators.required] }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(5)],
+    }),
+  });
 
-  constructor() { }
-
-  ngOnInit() {
+  startLogin() {
+    console.log(this.loginForm.value);
   }
-
 }
