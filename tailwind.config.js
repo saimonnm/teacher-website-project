1510733dfeee219dc000aebccefde701f89166d9
Simona Mangano <simona_mangano99@yahoo.it>
2026-05-03 22:16:24 +0200

    color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#9D8189",
+        secondary: "#F4ACB7",
+        accent: "#FFCAD4",
+        soft: "#FFE5D9",
+        background: "#D8E2DC",
+      },
+    },
   },
   plugins: [],
 };
